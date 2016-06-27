e0c0af890dc183cc1cee9b1980aaf990c48049c5
Paul Chaignon <paul.chaignon@gmail.com>
2016-06-27 19:10:10 +0200

    Turn the Filename strategy into a filter strategy
    Before, the Filename strategy only returned a number of candidate languages
    without using languages detected by previous strategies.
    
    Now, the Filename strategy acts as a filter on languages detected by previous strategies.
    If, all languages are filtered, then it returns the union of previously
    detected languages and languages detected by the Filename strategy.

--- lib/linguist/strategy/filename.rb
+++ lib/linguist/strategy/filename.rb
@@ -2,8 +2,14 @@ module Linguist
   module Strategy
     # Detects language based on filename and/or extension
     class Filename
-      def self.call(blob, _)
-        Language.find_by_filename(blob.name.to_s)
+      def self.call(blob, possible_languages)
+        detected_languages = Language.find_by_filename(blob.name.to_s)
+        languages = possible_languages & detected_languages
+        if languages.empty?
+          possible_languages | detected_languages
+        else
+          languages
+        end
       end
     end
   end
