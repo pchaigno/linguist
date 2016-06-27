bbf77a7fce7494ae53c7056137e55458ba47f5ef
Paul Chaignon <paul.chaignon@gmail.com>
2016-06-27 19:21:20 +0200

    node interpreter for TypeScript

---
+++ samples/TypeScript/alsatian-cli.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import { TestRunner, TestSet } from "../core/alsatian-core";
+
+// get all arguments from the user
+let userArguments = process.argv.slice(2);
+
+// filter out the file globs
+let fileGlobs = userArguments.filter((argument: string) => argument.indexOf("-") !== 0);
+
+// filter out the options
+let options = userArguments.filter((argument: string) => argument.indexOf("-") === 0);
+
+// create test set from given file globs
+let testSet = new TestSet(fileGlobs);
+
+// create runner and run those tests
+let testRunner = new TestRunner();
+testRunner.run(testSet);
